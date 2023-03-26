7a5d0bc2b2aa8721f6503edf08f07c1461a6d0f4
KovalVladyslav <vladxnos@gmail.com>
2023-03-26 20:29:54 +0300

    task 3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+let randomColor = function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
 
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+
+let timerId = null;
+stop.disabled = true;
+start.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = randomColor();
+  }, 1000);
+  start.disabled = true;
+  stop.disabled = false;
+});
+
+stop.addEventListener('click', () => {
+  clearInterval(timerId);
+  start.disabled = false;
+  stop.disabled = true;
+  document.body.style.backgroundColor = '#ffffff';
+});
